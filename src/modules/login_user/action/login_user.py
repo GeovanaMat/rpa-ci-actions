eabe66a8834cae8a24ecdb96bb2c62ca0f57662c
Geovana <geovana.matias1@gmail.com>
2026-05-20 15:34:43 -0400

    Ajuste de env githubactions

--- src/modules/login_user/action/login_user.py
+++ src/modules/login_user/action/login_user.py
@@ -1,16 +1,15 @@
 import os
 from src.shared.navigation import Navigation
-from src.modules.login_user.common.selectors import selectors
-from time import sleep
+
 class LoginUserPage:
     
     def __init__(self, navigation: Navigation):
         self.navigation = navigation
     
     def fill_login_form(self):
-        email = os.getenv("EMAIL")
-        password = os.getenv("PASSWORD")
-
+        email = os.getenv("LINKEDIN_EMAIL")
+        password = os.getenv("LINKEDIN_PASSWORD")
+        
         self.navigation.page.get_by_role("textbox", name="E-mail ou telefone").fill(email)
         self.navigation.page.get_by_role("textbox", name="Senha").fill(password)
         self.navigation.page.get_by_role("button", name="Entrar", exact=True).click()
